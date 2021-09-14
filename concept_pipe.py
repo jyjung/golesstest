ce2fb846b648cd0453e4d962b9ec31ebd17668cd
Jung <bungker@gmail.com>
2021-09-15 00:53:09 +0900

    code

--- concept_pipe.py
+++ concept_pipe.py
@@ -17,31 +17,58 @@ import time
 from threading import Thread
 import hashlib
 import os
+import uuid 
+import time
+from typing import Tuple,Dict,Any,Callable
 
 import goless
 import time
 import gevent
 
 
+gdict = {}
 
-def make_gen():
+def fake_begin_mip()-> str:
+    id = str(uuid.uuid4())
+    gdict[id] = time.time()
+    return id
+
+def fake_status( id: str)-> bool:
+    if id in gdict  and  time.time() - gdict[id] > 2:
+        return True
+    else:
+        return False
+
+def test1():
+    id = fake_begin_mip()
+    print(id)
+    print(gdict[id])
+    for i in range(5):
+        print(fake_status(id))
+        time.sleep(1)
+
+
+
+
+def make_gen(sleep_time: float):
     def _inner():
+        nonlocal sleep_time
         names = ["jung","kim","hong","bae","park"]
         for i in names:
-            gevent.sleep(1)
+            gevent.sleep(sleep_time)
             yield i
             
     return _inner
 
 #print(next(gen2()))
 def pipeline(index):
-    gen1 = make_gen()
-    gen2 = make_gen()
-    gen3 = make_gen()
+    gen1 = make_gen(0.5)
+    gen2 = make_gen(0.5)
+    gen3 = make_gen(1)
 
-    files = goless.chan(5)
-    hashes = goless.chan(5)
-    results = goless.chan(5)
+    files = goless.chan(1)
+    hashes = goless.chan(1)
+    results = goless.chan(1)
 
     def file_works():
         for i in gen1():
@@ -63,18 +90,25 @@ def pipeline(index):
             
         results.close()
 
+    def collector2():
+        for f in  hashes:
+            g= next(gen3())
+            n = f + "---" + g
+            results.send(n)
+            
+        results.close()
+
+
     goless.go(file_works)
     goless.go(simul_mip_api)
     goless.go(collector)
+    goless.go(collector2)    
 
     for f in results:
         print('[%d] %s' % (index, f))
 
-
-if __name__ == '__main__':
+def main1():
     start = time.time()
-    #pipeline(1)
-
     tlist =[]
     for i in range(5):
         t =Thread(target=pipeline, args=(i,))
@@ -84,5 +118,9 @@ if __name__ == '__main__':
     for t in tlist:
         t.join()
     print(time.time()-start)
+
+
+if __name__ == '__main__':
+    main1()
 
 
