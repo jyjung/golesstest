5f57a2766622d25f24d2625ffeae31a60859336a
Jung <bungker@gmail.com>
2021-09-26 03:27:45 +0900

    pipe store

--- item_store.py
+++ item_store.py
@@ -1,4 +1,7 @@
 import threading
+import signal
+from typing import List, Tuple, Dict, Optional
+from enum import Enum, IntEnum , auto
 
 class WorkingItem:
     def __init__(self,filename: str, id:str):
@@ -9,23 +12,97 @@ class WorkingItem:
 
 
 class ItemStore(object):
-    def __init__(self):
+    def __init__(self, name: str):
+        self.name = name
         self.cond = threading.Condition()
         self.items = []
+        self.flag = True
+
+    def loop(self):
+        return self.flag
+    
+    def stop_loop(self):
+        self.flag = False
 
     def add(self, item):
         with self.cond:
             self.items.append(item)
             self.cond.notify() # Wake 1 thread waiting on cond (if any)
 
-    def get_all(self, blocking=False):
+    def get_one(self, blocking=False , timeout=None ):
+        with self.cond:
+            while blocking and len(self.items) == 0:
+                b = self.cond.wait(timeout)
+                if timeout and timeout > 0 and not b:
+                    return None
+            item, self.items = self.items[0], self.items[1:]
+        return item
+
+    def get_all(self, blocking=False , timeout=None ):
         with self.cond:
             # If blocking is true, always return at least 1 item
             while blocking and len(self.items) == 0:
-                self.cond.wait()
+                #If timeout  cond.wait  return False  
+                b = self.cond.wait(timeout)
+                if timeout and timeout > 0 and not b:
+                    return []
             items, self.items = self.items, []
         return items
 
-if __name__ == '__main__':
+
+class PipeStore:
+    def __init__(self, enumclass) -> None:
+        signal.signal(signal.SIGINT,self.safe_exit)        
+        signal.signal(signal.SIGTERM,self.safe_exit)
+        self.len = len(enumclass)
+        #self.stores = [ ItemStore() for _ in range(self.len)]
+        self.stores= []
+        self.idxmap = {}
+        for idx , enum_item  in  enumerate(enumclass):
+            self.stores.append(ItemStore(enum_item.name))
+            self.idxmap[enum_item.name]= idx
+
+    def safe_exit(self, signum, frame):
+        print('safe_exit')
+        self.stores[0].stop_loop()
+
+    def get_store(self, enum_item) -> ItemStore:
+        idx = self.idxmap[enum_item.name]
+        if not 0 <= idx < self.len:
+            raise ValueError
+        return self.stores[idx]
+
+    def get_next_store(self, enum_item) -> ItemStore:
+        idx = self.idxmap[enum_item.name]
+        next_idx = idx +1 
+        if not 0 <= next_idx < self.len:
+            raise ValueError
+        return self.stores[next_idx]
+
+def main():
+    class MYOrder(Enum):
+        FILE_ENCODING = 9
+        FILE_UPLOAD = 7
+        AAA = 6
+        BBB = 19 
+    
+    mydict={}
+    for i,j in enumerate(MYOrder):
+        mydict[j.name] = i 
+
+    print(mydict)
+    
+    
     a =WorkingItem('fff','bbb')
     print(a.filename)
+
+def main2():
+    #mylen = [1,2,3,4,5,6]        
+    mylen = [1]        
+    
+    l1,l2 = mylen[0:1] , mylen[1:]
+    print(l1)
+    print(l2)
+    
+if __name__ == '__main__':
+    main2()
--- thread_pipe.py
+++ thread_pipe.py
@@ -7,11 +7,19 @@ import gevent
 import goless
 from threading import Thread
 from queue import Queue
+from enum import Enum, IntEnum ,auto
 from typing import Union
-from item_store import ItemStore , WorkingItem
+from item_store import ItemStore , WorkingItem , PipeStore
 from expiringdict import ExpiringDict
 
 cache_dict = ExpiringDict(max_len=1000, max_age_seconds=10)
+
+class ExecOrder(Enum):
+    FILE_DOWNLOAD =0
+    FILE_SANITIZE = 1
+    FILE_CHANGESIZE = 2
+    FILE_ENCODING = 3
+    FILE_UPLOAD = 4
 
 def fake_file_download()-> str:
     id = str(uuid.uuid4()) + ".docx"
@@ -33,11 +41,25 @@ def fake_file_encoding_status( id: str)-> bool:
     else:
         return False
 
-def wait_until_complete(item_in: ItemStore, item_out: ItemStore):
+def file_download_step(ps : PipeStore):
+    store = ps.get_store(ExecOrder.FILE_DOWNLOAD)
+    next_store = ps.get_next_store(ExecOrder.FILE_DOWNLOAD)    
+    while store.loop():
+        inlist = store.get_all(blocking=True,timeout=1)
+        for item in inlist:
+            print('download_item',str(item))
+
+    
+
+
+
+def file_encoding_step(ps : PipeStore):
     working_item_list = []
     remain_item_list =[]
-    while True:
-        inlist = item_in.get_all(blocking=False)
+    store = ps.get_store(ExecOrder.FILE_ENCODING)
+    next_store = ps.get_next_store(ExecOrder.FILE_ENCODING)
+    while store.loop():
+        inlist = store.get_all(blocking=False)
         new_item_list = [fake_file_encoding_start(v) for v in inlist]
         working_item_list = remain_item_list + new_item_list
         remain_item_list.clear()
@@ -45,16 +67,24 @@ def wait_until_complete(item_in: ItemStore, item_out: ItemStore):
             # 실제 구현시 pending 되는것만 remain에다가 넣고 
             # 기타는 결과값을 확인한 후에 다시 집어 넣으면 된다. 
             if fake_file_encoding_status(item.id):
-                item_out.add(item)
+                # 다음 처리를 위한 저장소로 넘겨준다  
+                next_store.add(item)
             else:
                 remain_item_list.append(item)
         time.sleep(0.5)
+    next_store.stop_loop()
+    print('file encoding end')
+    
 
-def print_item(item_in: ItemStore):
-    while True:
-        inlist = item_in.get_all(blocking=True)
+def file_upload_step(ps: PipeStore):
+    store = ps.get_store(ExecOrder.FILE_UPLOAD)
+    while store.loop():
+        inlist = store.get_all(blocking=True,timeout=3)
         for item in inlist:
             print('print_item',str(item))
+    print('file upload end')
+        
+    # exit next step
 
 def queue_test():
     q = ItemStore()
@@ -64,32 +94,25 @@ def queue_test():
     print('1',q.get_all())
     print('2',q.get_all())
 
-def pipe(item: ItemStore):
-    while True:
-        my = item.get_all(blocking=True)
-        print('my',my)
-        time.sleep(3)
-    pass
 
 def main():
-
-    fd_in = ItemStore()
-    fd_out = ItemStore()
+    ps = PipeStore(ExecOrder)
     for i in range(2):
-        t =Thread(target=wait_until_complete, args=(fd_in,fd_out,))
+        t =Thread(target=file_encoding_step, args=(ps,))
         t.daemon = True
         t.start()
-    t2 = Thread(target=print_item, args=(fd_out,))
+    t2 = Thread(target=file_upload_step, args=(ps,))
     t2.daemon = True
     t2.start()
 
-    fd_in.add('first1_file')
+    store = ps.get_store(ExecOrder.FILE_ENCODING)
+    store.add('first1_file')
     time.sleep(2)        
-    fd_in.add('first2_file')    
-    fd_in.add('first2_1_file')
-    fd_in.add('first2_2_file')
+    store.add('first2_file')    
+    store.add('first2_1_file')
+    store.add('first2_2_file')
     time.sleep(2)        
-    fd_in.add('first3_file')        
+    store.add('first3_file')        
     while True:
         time.sleep(1)
 
