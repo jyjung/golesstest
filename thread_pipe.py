f4fdd7de489fb48ad9c91ab5be6dd50f8f016ec5
Jung <bungker@gmail.com>
2021-09-26 22:57:54 +0900

    modify main

--- thread_pipe.py
+++ thread_pipe.py
@@ -17,7 +17,7 @@ cache_dict = ExpiringDict(max_len=1000, max_age_seconds=10)
 class ExecOrder(Enum):
     FILE_DOWNLOAD =0
     FILE_SANITIZE = 1
-    FILE_CHANGESIZE = 2
+    FILE_RESIZE = 2
     FILE_ENCODING = 3
     FILE_UPLOAD = 4
 
@@ -41,31 +41,31 @@ def fake_file_encoding_status( id: str)-> bool:
     else:
         return False
 
-def base_step(ps : PipeStore, enum):
+def base_step(ps : PipeStore, enum , sleep_time):
     store = ps.get_store(enum)
     next_store = ps.get_next_store(enum)    
     while store.loop():
         item = store.get_one(blocking=True,timeout=1)
-        print(enum.name,str(item))
-        # item에다가 새로운 자료를 담는다. 
-        time.sleep(1)
-        if next_store:
-            next_store.add(item)
-    if next_store:            
+        if item:
+            print(enum.name,str(item))
+            # item에다가 새로운 자료를 담는다. 
+            time.sleep(sleep_time)
+            if next_store:
+                next_store.add(item)
+    if next_store:     
+        print(next_store.name,'loop exit')       
         next_store.stop_loop()
 
 
 
 def file_download_step(ps : PipeStore):
-    store = ps.get_store(ExecOrder.FILE_DOWNLOAD)
-    next_store = ps.get_next_store(ExecOrder.FILE_DOWNLOAD)    
-    while store.loop():
-        item = store.get_one(blocking=True,timeout=1)
-        print('download_item',str(item))
-        time.sleep(1)
-        next_store.add(item)
-    next_store.stop_loop()
-    
+    base_step(ps,ExecOrder.FILE_DOWNLOAD,1)
+
+def file_sanitize_step(ps : PipeStore):
+    base_step(ps,ExecOrder.FILE_SANITIZE,0.5)
+
+def file_resize_step(ps : PipeStore):
+    base_step(ps,ExecOrder.FILE_RESIZE,0.8)
 
     
 
@@ -76,7 +76,7 @@ def file_encoding_step(ps : PipeStore):
     remain_item_list =[]
     store = ps.get_store(ExecOrder.FILE_ENCODING)
     next_store = ps.get_next_store(ExecOrder.FILE_ENCODING)
-    while store.loop():
+    while store.loop() or len(working_item_list) > 0:
         inlist = store.get_all(blocking=False)
         new_item_list = [fake_file_encoding_start(v) for v in inlist]
         working_item_list = remain_item_list + new_item_list
@@ -117,19 +117,26 @@ def main():
     ps = PipeStore(ExecOrder)
     for i in range(2):
         t =Thread(target=file_encoding_step, args=(ps,))
-        t.daemon = True
         t.start()
     t2 = Thread(target=file_upload_step, args=(ps,))
-    t2.daemon = True
     t2.start()
 
-    store = ps.get_store(ExecOrder.FILE_ENCODING)
+    t3 = Thread(target=file_download_step, args=(ps,))
+    t3.start()
+
+    t4 = Thread(target=file_sanitize_step, args=(ps,))
+    t4.start()
+
+    t5 = Thread(target=file_resize_step, args=(ps,))
+    t5.start()
+
+
+
+    store = ps.get_store(ExecOrder.FILE_DOWNLOAD)
     store.add('first1_file')
-    time.sleep(2)        
     store.add('first2_file')    
     store.add('first2_1_file')
     store.add('first2_2_file')
-    time.sleep(2)        
     store.add('first3_file')        
     while True:
         time.sleep(1)
